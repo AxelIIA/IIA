38d2365a9c2a4526991e684abec7aa3e5f8b0670
AxelIIA <AxelIIA@axel.neau-paye@iia-formation.fr>
2025-04-01 14:13:35 +0200

    backend

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(cors())
+app.use(express.json({
+    limit: '10mb'
+}))
+
+// Quand il reçoit une requête visant cet endpoint
+// http://localhost:3001/api/data
+app.post('/api/data', (req, res) => {
+    // const { ip } = req
+    const { email, password } = req.body
+    console.log(email, password)
+    res.status(200)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`✅ Server running at ${process.env.PORT}`)
+})
+

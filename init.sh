995c1007455f42cb72a6b1d329eb6286456020ac
AxelIIA <AxelIIA@axel.neau-paye@iia-formation.fr>
2025-04-02 11:45:10 +0200

    ajout db et dependences

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+npm install bcrypt jsonwebtoken express-validator 
+npm sequelize pg pg-hstore
--- server.js
+++ server.js
@@ -12,14 +12,21 @@ app.use(cors())
 app.use(express.json({
     limit: '10mb'
 }))
+app.set('port', process.env.PORT);
+app.set('host', process.env.HOST);
+
 
 // Quand il reçoit une requête visant cet endpoint
-// https://iia-front.onrender.com//api/data
+// https://iia-backend.onrender.com//api/data
 app.post('/api/data', (req, res) => {
     // const { ip } = req
     const { email, password } = req.body
     console.log(email, password)
     res.status(200).json({"message": "connexion réuissie"})
+})
+
+app.listen(app.get('port'), function(){
+    console.log(`Server running at ${app.get('host')} :  ${app.get('port')}`)
 })
 
 app.listen(process.env.PORT, () => {
